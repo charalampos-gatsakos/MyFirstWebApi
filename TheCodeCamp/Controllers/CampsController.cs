45ed6da0c9a0b9081881ea8be3537a4b5b0c2b38
Daddy-Gats <charalamposgatsakos@gmail.com>
2022-03-13 19:49:36 +0200

    Camp Controller Ud

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        // GET: Camps
+        public IHttpActionResult Get()
+        {
+            return BadRequest("It's only a game");
+            return Ok(new { Name = "Babis", Occupation = "Developer" });
+        }
+    }
+}
